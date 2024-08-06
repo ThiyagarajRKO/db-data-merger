2245f0883490b93194f7dd479c48761b391bdb39
ThiyagarajRKO <ThiyagarajRKO@gmail.com>
2024-08-06 06:28:40 +0530

    copy two table data into new table script added - final

--- index.js
+++ index.js
@@ -28,17 +28,23 @@ const getColumns = async (connection, tableName) => {
   return results.map((row) => row.Field);
 };
 
-const getPrimaryKey = async (connection, tableName) => {
-  const [results] = await connection.query(`SELECT COLUMN_NAME
-  FROM INFORMATION_SCHEMA.COLUMNS
-  WHERE TABLE_SCHEMA = '${db2Config.database}'
-  AND TABLE_NAME = '${tableName}'
-  AND COLUMN_KEY = 'PRI'`);
+const getAutoIncCol = async (connection, tableName) => {
+  const [results] = await connection.query(`SELECT 
+    COLUMN_NAME 
+FROM 
+    information_schema.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = '${db3Config.database}' 
+    AND TABLE_NAME = '${tableName}'
+    AND EXTRA LIKE '%auto_increment%';
+`);
   return results.map((row) => row.COLUMN_NAME);
 };
 
-const copyData = async (db1Connection, db2Connection, db3Connection, table) => {
+const copyData = async (db2Connection, db3Connection, table) => {
   const columns = await getColumns(db2Connection, table);
+
+  const autIncCol = await getAutoIncCol(db2Connection, table);
 
   // Create the column list and placeholders for the SQL query
   let columnsList = columns.join("`,`");
@@ -47,10 +53,10 @@ const copyData = async (db1Connection, db2Connection, db3Connection, table) => {
       if (col == "submit_date") {
         return `CASE WHEN submit_date = '0000-00-00' THEN NULL ELSE submit_date END as submit_date`;
       } else {
-        return `${col}`;
+        return `\`${col}\``;
       }
     })
-    .join(", ");
+    .join(",");
 
   // Disable foreign key checks (if necessary)
   const disableFKChecks = `SET FOREIGN_KEY_CHECKS=0`;
@@ -58,13 +64,17 @@ const copyData = async (db1Connection, db2Connection, db3Connection, table) => {
   const disableStrictMode = `SET SESSION sql_mode = ''`;
 
   // Query to create the new table (if not exists)
+  const dropTableQuery = `
+        DROP TABLE IF EXISTS \`${table}\``;
+
+  // Query to create the new table (if not exists)
   const createTableQuery = `
-        CREATE TABLE IF NOT EXISTS \`${table}\` LIKE \`${db1Config.database}\`.\`${table}\`;
+        CREATE TABLE \`${table}\` LIKE \`${db1Config.database}\`.\`${table}\`;
     `;
 
   // Disable auto increment temporarily
   const disableAutoIncrement = `
-        ALTER TABLE \`${table}\` MODIFY COLUMN id INT NOT NULL;
+        ALTER TABLE \`${table}\` MODIFY COLUMN ${autIncCol[0]} INT NOT NULL;
     `;
 
   // Query to copy data from table1 to new_table
@@ -83,7 +93,7 @@ const copyData = async (db1Connection, db2Connection, db3Connection, table) => {
 
   // Re-enable auto increment on the id column
   const enableAutoIncrement = `
-        ALTER TABLE \`${table}\` MODIFY COLUMN id INT NOT NULL AUTO_INCREMENT;
+        ALTER TABLE \`${table}\` MODIFY COLUMN ${autIncCol[0]} INT NOT NULL AUTO_INCREMENT;
     `;
 
   // Re-enable foreign key checks
@@ -96,9 +106,11 @@ const copyData = async (db1Connection, db2Connection, db3Connection, table) => {
 
   await db3Connection.query(disableStrictMode);
 
+  await db3Connection.query(dropTableQuery);
+
   await db3Connection.query(createTableQuery);
 
-  await db3Connection.query(disableAutoIncrement);
+  if (autIncCol[0]) await db3Connection.query(disableAutoIncrement);
 
   const [results] = await db3Connection.query(copyFromTable1Query);
   console.log("Table 1 Rows inserted:", results.affectedRows);
@@ -106,7 +118,7 @@ const copyData = async (db1Connection, db2Connection, db3Connection, table) => {
   const [result1] = await db3Connection.query(copyFromTable2Query);
   console.log("Table 2 Rows inserted:", result1.affectedRows);
 
-  await db3Connection.query(enableAutoIncrement);
+  if (autIncCol[0]) await db3Connection.query(enableAutoIncrement);
 
   await db3Connection.query(enableFKChecks);
 
@@ -114,32 +126,33 @@ const copyData = async (db1Connection, db2Connection, db3Connection, table) => {
 };
 
 const compareDatabases = async () => {
-  const db1Connection = await mysql.createConnection(db1Config);
+  //   const db1Connection = await mysql.createConnection(db1Config);
   const db2Connection = await mysql.createConnection(db2Config);
   const db3Connection = await mysql.createConnection(db3Config);
 
   try {
     const [tables1, tables2] = await Promise.all([
-      db1Connection.query("SHOW TABLES"),
+      //   db1Connection.query("SHOW TABLES"),
       db2Connection.query("SHOW TABLES"),
     ]);
 
-    const tableNames1 = tables1[0].map((row) => Object.values(row)[0]);
+    // const tableNames1 = tables1[0].map((row) => Object.values(row)[0]);
     const tableNames2 = tables2[0].map((row) => Object.values(row)[0]);
 
-    const commonTables = tableNames1.filter((table) =>
-      tableNames2.includes(table)
-    );
+    // const commonTables = tableNames1.filter((table) =>
+    //   tableNames2.includes(table)
+    // );
 
-    for (const table of commonTables) {
+    for (const table of tableNames2) {
       console.log("Processing Table:", table);
-      await copyData(db1Connection, db2Connection, db3Connection, table);
+      await copyData(db2Connection, db3Connection, table);
     }
   } catch (err) {
     console.error("Error:", err.message);
   } finally {
-    await db1Connection.end();
+    // await db1Connection.end();
     await db2Connection.end();
+    await db3Connection.end();
   }
 };
 
