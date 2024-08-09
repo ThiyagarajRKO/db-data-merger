f138848530ea02fa5e62fcb5c0b90905249a4082
ThiyagarajRKO <ThiyagarajRKO@gmail.com>
2024-08-10 02:00:10 +0530

    added ent and user data sync

--- index.js
+++ index.js
@@ -337,28 +337,48 @@ const syncTrainees = async (
     try {
       const columns = await getColumns(db1Connection, "trainees");
 
-      // Create the column list and placeholders for the SQL query
-      const placeholders = columns
-        .map((col) => {
-          if (col == "submit_date") {
-            return `CASE WHEN submit_date = '0000-00-00' THEN NULL ELSE submit_date END as submit_date`;
-          } else if (col == "annual_action_plan_id") {
-            return `'${new_annual_plan_id}'`;
-          } else if (col == "training_title_id") {
-            return `'${new_training_title_id}'`;
-          } else {
-            return `\`${col}\``;
-          }
-        })
-        .join(",")
-        .replace("`id`,", "");
-
       const [results] = await db2Connection.query(
-        `SELECT id FROM trainees where financial_year_id = ${financial_year_id} and annual_action_plan_id = ${old_annual_plan_id} and training_title_id = ${old_training_title_id}`
+        `SELECT id, entrepreneur_id, user_id FROM trainees where financial_year_id = ${financial_year_id} and annual_action_plan_id = ${old_annual_plan_id} and training_title_id = ${old_training_title_id}`
       );
 
       for (const row of results) {
         const id = row["id"];
+        const user_id = row["user_id"];
+        const entrepreneur_id = row["entrepreneur_id"];
+
+        const new_user_id = await syncUser(
+          db1Connection,
+          db2Connection,
+          user_id
+        );
+
+        const new_entrepreneur_id = await syncEntrepreneurs(
+          db1Connection,
+          db2Connection,
+          entrepreneur_id,
+          new_user_id
+        );
+
+        // Create the column list and placeholders for the SQL query
+        const placeholders = columns
+          .map((col) => {
+            if (col == "submit_date") {
+              return `CASE WHEN submit_date = '0000-00-00' THEN NULL ELSE submit_date END as submit_date`;
+            } else if (col == "annual_action_plan_id") {
+              return `'${new_annual_plan_id}'`;
+            } else if (col == "training_title_id") {
+              return `'${new_training_title_id}'`;
+            } else if (col == "entrepreneur_id") {
+              return `'${new_entrepreneur_id}'`;
+            } else if (col == "user_id") {
+              return `'${new_user_id}'`;
+            } else {
+              return `\`${col}\``;
+            }
+          })
+          .join(",")
+          .replace("`id`,", "");
+
         // Query to copy data from table2 to new_table
         const query = `INSERT INTO trainees (${columns.join("`,`").replace("id`,", "")}\`) SELECT ${placeholders} FROM \`${db2Config.database}\`.trainees WHERE id = ${id};`;
 
@@ -368,6 +388,99 @@ const syncTrainees = async (
       }
 
       resolve();
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+const syncUser = async (db1Connection, db2Connection, old_user_id) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const columns = await getColumns(db1Connection, "users");
+
+      // Create the column list and placeholders for the SQL query
+      const placeholders = columns
+        .map((col) => `\`${col}\``)
+        .join(",")
+        .replace("`id`,", "");
+
+      const [user_data] = await db2Connection.query(
+        `SELECT email FROM users where id = ${old_user_id}`
+      );
+
+      if (!user_data[0]?.email) {
+        return reject("Base data doesn't exist.");
+      }
+
+      const [user_result] = await db1Connection.query(
+        `SELECT id FROM users where email = '${user_data[0].email}'`
+      );
+
+      if (user_result[0]?.id) {
+        return resolve(user_result[0]?.id);
+      }
+
+      const query = `INSERT INTO users (${columns.join("`,`").replace("id`,", "")}\`) SELECT ${placeholders} FROM \`${db2Config.database}\`.users WHERE email = '${user_data[0]?.email}'`;
+
+      const [results] = await db1Connection.query(query);
+
+      resolve(results.insertId);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+const syncEntrepreneurs = async (
+  db1Connection,
+  db2Connection,
+  old_entrepreneur_id,
+  new_user_id
+) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const columns = await getColumns(db1Connection, "entrepreneurs");
+
+      // Create the column list and placeholders for the SQL query
+      const placeholders = columns
+        .map((col) => {
+          if (col == "user_id") {
+            return `'${new_user_id}'`;
+          } else {
+            return `\`${col}\``;
+          }
+        })
+        .join(",")
+        .replace("`id`,", "");
+
+      const [ent_data] = await db2Connection.query(
+        `SELECT id, email FROM entrepreneurs where id = ${old_entrepreneur_id}`
+      );
+
+      if (!ent_data[0]?.email) {
+        return reject("Base data doesn't exist.");
+      }
+
+      const [ent_result] = await db1Connection.query(
+        `SELECT id, user_id FROM entrepreneurs where email = '${ent_data[0]?.email}'`
+      );
+
+      if (ent_result[0]?.id) {
+        if (ent_result[0]?.user_id != new_user_id) {
+          const query = `UPDATE entrepreneurs SET user_id = '${new_user_id}' WHERE id = ${old_entrepreneur_id}`;
+
+          await db1Connection.query(query);
+        }
+
+        return resolve(old_entrepreneur_id);
+      }
+
+      const query = `INSERT INTO entrepreneurs (${columns.join("`,`").replace("id`,", "")}\`) SELECT ${placeholders} FROM \`${db2Config.database}\`.entrepreneurs WHERE email = '${ent_data[0]?.email}'`;
+
+      const [results] = await db1Connection.query(query);
+
+      resolve(results.insertId);
     } catch (error) {
       reject(error);
     }
